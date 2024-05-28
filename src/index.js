2e1b80bf585c9c14b14ab66acda9605616b05d49
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-05-28 21:29:04 +0300

    Add the function that hitthe api, with my api key

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 import './style.css';
+
+async function weather(city) {
+  const response = await fetch(
+    `http://api.weatherapi.com/v1/current.json?key=cd485184bc0d44e5b67172805242305&q=${city}`
+  );
+  const data = await response.json();
+  console.log(data);
+}
+
+weather('london');
+
