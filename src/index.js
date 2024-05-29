5a45085083d9b2b4f09fef1f5b2c44eb708b423b
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-05-29 21:22:26 +0300

    Set the func in a async/await format

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import './style.css';
 
 async function weather(city) {
-  const response = await fetch(
-    `http://api.weatherapi.com/v1/current.json?key=cd485184bc0d44e5b67172805242305&q=${city}`
-  );
-  const data = await response.json();
-  console.log(data);
+  try {
+    const response = await fetch(
+      `http://api.weatherapi.com/v1/current.json?key=cd485184bc0d44e5b67172805242305&q=${city}`
+    );
+    if (response.status) {
+      const data = await response.json();
+      console.log(data);
+    } else {
+      throw new Error(`${response.status}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 weather('london');
-
