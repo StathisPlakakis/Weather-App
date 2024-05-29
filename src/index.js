3477aacea2b247ed41fce07260fc162ee376870b
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-05-29 21:33:14 +0300

    Debug func to catch http errors

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ async function weather(city) {
     const response = await fetch(
       `http://api.weatherapi.com/v1/current.json?key=cd485184bc0d44e5b67172805242305&q=${city}`
     );
-    if (response.status) {
+    if (response.ok) {
       const data = await response.json();
       console.log(data);
     } else {
