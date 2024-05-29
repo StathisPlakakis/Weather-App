2856b96776ce32d86e2523ba0a08905f7935f82f
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-05-29 21:49:23 +0300

    Add cors option in fetch invocation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import './style.css';
 async function weather(city) {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=cd485184bc0d44e5b67172805242305&q=${city}`
+      `http://api.weatherapi.com/v1/current.json?key=cd485184bc0d44e5b67172805242305&q=${city}`,
+      {
+        mode: 'cors',
+      }
     );
     if (response.ok) {
       const data = await response.json();
