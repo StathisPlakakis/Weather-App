cf1fbf0e0c12a915c791e5ce430919d21232488d
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-05-29 22:30:24 +0300

    Modularize weather function to a separate file

--- src/index.js
+++ src/index.js
@@ -1,24 +1,6 @@
 import './style.css';
+import weather from './data';
 
-async function weather(city) {
-  try {
-    const response = await fetch(
-      `http://api.weatherapi.com/v1/current.json?key=cd485184bc0d44e5b67172805242305&q=${city}`,
-      {
-        mode: 'cors',
-      }
-    );
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data);
-      return data;
-    } else {
-      throw new Error(`${response.status}`);
-    }
-  } catch (error) {
-    console.log(error);
-    return error.message;
-  }
-}
 
-weather('londmfmon');
+
+weather('london');
