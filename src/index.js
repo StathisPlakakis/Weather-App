203d9d6fcef54a58233043999bcc1d414ad0ff9b
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-06-01 00:57:13 +0300

    Add keyup event when Enter key is pressed

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
 import './style.css';
-import { weather } from './weather/data'; 
+import { weather } from './weather/data';
 
-weather('greece')
-  .then((result) => {
-    console.log(result);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
+// weather('athens')
+//   .then((result) => {
+//     console.log(result);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
+
+const input = document.querySelector('input');
+const search = document.querySelector('.search-action');
+
+search.addEventListener('click', () => {
+  weather(input.value)
+    .then((result) => {
+      console.log(result);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+input.addEventListener('keyup', (e) => {
+  if (e.key === 'Enter') {
+    weather(input.value)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+});
