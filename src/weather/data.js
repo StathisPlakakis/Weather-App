003e1809f2ab79fee96c19a4e1438a595093c522
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-05-31 08:00:30 +0300

    Add func for weather forcast details

--- src/weather/data.js
+++ src/weather/data.js
@@ -18,7 +18,7 @@ async function weatherCurrent(city) {
       const condition = data.current.condition.text;
       const city = data.location.name;
       const country = data.location.country;
-      const specificData = {
+      const specificDataCurrent = {
         temperatureC,
         temperatureF,
         windSpeedC,
@@ -30,8 +30,8 @@ async function weatherCurrent(city) {
         city,
         country,
       };
-      console.log(specificData);
-      return specificData;
+      console.log(specificDataCurrent);
+      return specificDataCurrent;
     } else {
       throw new Error(`A ${response.status}, error was thrown!`);
     }
@@ -51,7 +51,68 @@ async function weatherForcast(city) {
     );
     if (response.ok) {
       const data = await response.json();
-      console.log(data);
+      const $00_00c = data.forecast.forecastday[0].hour[0].temp_c;
+      const $00_00f = data.forecast.forecastday[0].hour[0].temp_f;
+      const $00_00_condition =
+        data.forecast.forecastday[0].hour[0].condition.text;
+      const $04_00c = data.forecast.forecastday[0].hour[4].temp_c;
+      const $04_00f = data.forecast.forecastday[0].hour[4].temp_f;
+      const $04_00_condition =
+        data.forecast.forecastday[0].hour[4].condition.text;
+      const $08_00c = data.forecast.forecastday[0].hour[8].temp_c;
+      const $08_00f = data.forecast.forecastday[0].hour[8].temp_f;
+      const $08_00_condition =
+        data.forecast.forecastday[0].hour[8].condition.text;
+      const $12_00c = data.forecast.forecastday[0].hour[12].temp_c;
+      const $12_00f = data.forecast.forecastday[0].hour[12].temp_f;
+      const $12_00_condition =
+        data.forecast.forecastday[0].hour[12].condition.text;
+      const $16_00c = data.forecast.forecastday[0].hour[16].temp_c;
+      const $16_00f = data.forecast.forecastday[0].hour[16].temp_f;
+      const $16_00_condition =
+        data.forecast.forecastday[0].hour[16].condition.text;
+      const $20_00c = data.forecast.forecastday[0].hour[20].temp_c;
+      const $20_00f = data.forecast.forecastday[0].hour[20].temp_f;
+      const $20_00_condition =
+        data.forecast.forecastday[0].hour[20].condition.text;
+      const $23_00c = data.forecast.forecastday[0].hour[23].temp_c;
+      const $23_00f = data.forecast.forecastday[0].hour[23].temp_f;
+      const $23_00_condition =
+        data.forecast.forecastday[0].hour[23].condition.text;
+      const maxTempC = data.forecast.forecastday[0].day.maxtemp_c;
+      const maxTempF = data.forecast.forecastday[0].day.maxtemp_f;
+      const minTempC = data.forecast.forecastday[0].day.mintemp_c;
+      const minTempF = data.forecast.forecastday[0].day.mintemp_f;
+      const specificDataForecast = {
+        $00_00c,
+        $00_00f,
+        $00_00_condition,
+        $04_00c,
+        $04_00f,
+        $04_00_condition,
+        $08_00c,
+        $08_00f,
+        $08_00_condition,
+        $12_00c,
+        $12_00f,
+        $12_00_condition,
+        $16_00c,
+        $16_00f,
+        $16_00_condition,
+        $20_00c,
+        $20_00f,
+        $20_00_condition,
+        $23_00c,
+        $23_00f,
+        $23_00_condition,
+        maxTempC,
+        maxTempF,
+        minTempC,
+        minTempF,
+      };
+
+      console.log(specificDataForecast);
+      return specificDataForecast;
     } else {
       throw new Error(`Hey my friend, you have a ${response.status} error!`);
     }
@@ -61,4 +122,4 @@ async function weatherForcast(city) {
   }
 }
 
-export  {weatherCurrent, weatherForcast};
+export { weatherCurrent, weatherForcast };
