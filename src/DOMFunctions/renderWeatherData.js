386e85fcaf3c3754a27258d26568c87e116c62f9
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-06-02 11:14:34 +0300

    Add data about city and country

--- src/DOMFunctions/renderWeatherData.js
+++ src/DOMFunctions/renderWeatherData.js
@@ -1,7 +1,11 @@
 function renderAllData(result) {
   const condition = document.querySelector('.condition');
   condition.textContent = result.condition;
-  condition.classList.add('visible');
+
+  const city = result.city;
+  const country = result.country;
+  const city_country = document.querySelector('.city-country');
+  city_country.textContent = `${city}, ${country}`
 }
 
 export default renderAllData;
