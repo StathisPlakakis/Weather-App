6d71d6f301f4f0a6c075252c9e2e4d8e128381d2
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-06-03 23:25:06 +0300

    Add icons for hourly forecast

--- src/weather/data.js
+++ src/weather/data.js
@@ -60,30 +60,44 @@ async function weatherForcast(city) {
       const $00_00f = data.forecast.forecastday[0].hour[0].temp_f;
       const $00_00_condition =
         data.forecast.forecastday[0].hour[0].condition.text;
+      const $00_00_condition_img =
+        data.forecast.forecastday[0].hour[0].condition.icon;
       const $04_00c = data.forecast.forecastday[0].hour[4].temp_c;
       const $04_00f = data.forecast.forecastday[0].hour[4].temp_f;
       const $04_00_condition =
         data.forecast.forecastday[0].hour[4].condition.text;
+      const $04_00_condition_img =
+        data.forecast.forecastday[0].hour[4].condition.icon;
       const $08_00c = data.forecast.forecastday[0].hour[8].temp_c;
       const $08_00f = data.forecast.forecastday[0].hour[8].temp_f;
       const $08_00_condition =
         data.forecast.forecastday[0].hour[8].condition.text;
+      const $08_00_condition_img =
+        data.forecast.forecastday[0].hour[8].condition.icon;
       const $12_00c = data.forecast.forecastday[0].hour[12].temp_c;
       const $12_00f = data.forecast.forecastday[0].hour[12].temp_f;
       const $12_00_condition =
         data.forecast.forecastday[0].hour[12].condition.text;
+      const $12_00_condition_img =
+        data.forecast.forecastday[0].hour[12].condition.icon;
       const $16_00c = data.forecast.forecastday[0].hour[16].temp_c;
       const $16_00f = data.forecast.forecastday[0].hour[16].temp_f;
       const $16_00_condition =
         data.forecast.forecastday[0].hour[16].condition.text;
+      const $16_00_condition_img =
+        data.forecast.forecastday[0].hour[16].condition.icon;
       const $20_00c = data.forecast.forecastday[0].hour[20].temp_c;
       const $20_00f = data.forecast.forecastday[0].hour[20].temp_f;
       const $20_00_condition =
         data.forecast.forecastday[0].hour[20].condition.text;
+      const $20_00_condition_img =
+        data.forecast.forecastday[0].hour[20].condition.icon;
       const $23_00c = data.forecast.forecastday[0].hour[23].temp_c;
       const $23_00f = data.forecast.forecastday[0].hour[23].temp_f;
       const $23_00_condition =
         data.forecast.forecastday[0].hour[23].condition.text;
+      const $23_00_condition_img =
+        data.forecast.forecastday[0].hour[23].condition.icon;
       const maxTempC = data.forecast.forecastday[0].day.maxtemp_c;
       const maxTempF = data.forecast.forecastday[0].day.maxtemp_f;
       const minTempC = data.forecast.forecastday[0].day.mintemp_c;
@@ -92,24 +106,31 @@ async function weatherForcast(city) {
         $00_00c,
         $00_00f,
         $00_00_condition,
+        $00_00_condition_img,
         $04_00c,
         $04_00f,
         $04_00_condition,
+        $04_00_condition_img,
         $08_00c,
         $08_00f,
         $08_00_condition,
+        $08_00_condition_img,
         $12_00c,
         $12_00f,
         $12_00_condition,
+        $12_00_condition_img,
         $16_00c,
         $16_00f,
         $16_00_condition,
+        $16_00_condition_img,
         $20_00c,
         $20_00f,
         $20_00_condition,
+        $20_00_condition_img,
         $23_00c,
         $23_00f,
         $23_00_condition,
+        $23_00_condition_img,
         maxTempC,
         maxTempF,
         minTempC,
