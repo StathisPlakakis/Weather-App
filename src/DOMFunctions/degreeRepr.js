11e4a7c111f11756dc36d9cd1d71ad6659e94189
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-06-03 23:11:02 +0300

    Debug changeBackgroundColor to render properly

--- src/DOMFunctions/degreeRepr.js
+++ src/DOMFunctions/degreeRepr.js
@@ -1,11 +1,15 @@
 import { getState } from './state';
 import renderAllData from './renderWeatherData';
 
-function changeBackgroundColor() {
+function changeBackgroundColor(e) {
+  const element = e.target;
   const buttons = document.querySelectorAll('.c-f button');
   buttons.forEach((button) => {
-    button.classList.toggle('selected');
+    if (button.classList.contains('selected')) {
+      button.classList.remove('selected');
+    }
   });
+  element.classList.add('selected');
   const result = getState();
   if (result) {
     renderAllData(result);
