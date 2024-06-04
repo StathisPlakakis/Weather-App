6ee2965854e695cd5b23d7e8dc9c7fc6765b0d54
StathisPlakakis <stathisplakorg1997@gmail.com>
2024-06-04 19:07:23 +0300

    Add loading gif, while fetching data

--- src/DOMFunctions/searchEvent.js
+++ src/DOMFunctions/searchEvent.js
@@ -6,22 +6,28 @@ import {
 } from './errorMessage';
 import renderAllData from './renderWeatherData';
 import { setState } from './state';
+import loadingGif from '../assets/Data/loading.gif';
 
-function searchWeatherFor(city) {
-  weather(city)
-    .then((result) => {
-      clearErrorMessage();
-      renderAllData(result);
-      setState(result);
-      console.log(result);
-    })
-    .catch((error) => {
-      if (error.name === 'TypeError') {
-        createNetworkErrorMessage();
-      } else {
-        createServerErrorMessage();
-      }
-    });
+async function searchWeatherFor(city) {
+  try {
+    const loading = document.createElement('img');
+    loading.src = `${loadingGif}`;
+    loading.classList.add('loading');
+    document.body.appendChild(loading);
+    const weatherData = await weather(city);
+    document.body.removeChild(loading);
+    clearErrorMessage();
+    renderAllData(weatherData);
+    setState(weatherData);
+  } catch (error) {
+    const loading = document.querySelector('.loading');
+    document.body.removeChild(loading);
+    if (error.name === 'TypeError') {
+      createNetworkErrorMessage();
+    } else {
+      createServerErrorMessage();
+    }
+  }
 }
 
 export default searchWeatherFor;
